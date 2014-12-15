8afb159631245ca2e2255863cfab8efb4fa811e8
Scott Vickers <scott.w.vickers@gmail.com>
2014-12-15 15:59:28 -0800

    update to yeoman-generator v0.17, refactor to use built in priorities and allow composability add a destination option for specifying a target folder update tests, arguments weren't being applied... add two new tests for destination option

--- test/test-file-creation.js
+++ test/test-file-creation.js
@@ -21,7 +21,8 @@ describe('H5BP Server Configs generator', function () {
 	it('`apache` option generates expected files', function (cb) {
 		var expected = [ path.join('.htaccess') ];
 
-		this.generator.run(['apache'], function () {
+		this.generator.server = 'apache';
+		this.generator.run({}, function () {
 			helpers.assertFiles(expected);
 			cb();
 		});
@@ -30,7 +31,8 @@ describe('H5BP Server Configs generator', function () {
 	it('`gae` option generates expected files', function (cb) {
 		var expected = [ 'app.yaml' ];
 
-		this.generator.run(['gae'], function () {
+		this.generator.server = 'gae';
+		this.generator.run({}, function () {
 			helpers.assertFiles(expected);
 			cb();
 		});
@@ -39,7 +41,8 @@ describe('H5BP Server Configs generator', function () {
 	it('`iis` option generates expected files', function (cb) {
 		var expected = [ 'dotnet 3', 'dotnet 4' ];
 
-		this.generator.run(['iis'], function () {
+		this.generator.server = 'iis';
+		this.generator.run({}, function () {
 			helpers.assertFiles(expected);
 			cb();
 		});
@@ -48,7 +51,8 @@ describe('H5BP Server Configs generator', function () {
 	it('`lighttpd` option generates expected files', function (cb) {
 		var expected = [ 'lighttpd.conf' ];
 
-		this.generator.run(['lighttpd'], function () {
+		this.generator.server = 'lighttpd';
+		this.generator.run({}, function () {
 			helpers.assertFiles(expected);
 			cb();
 		});
@@ -57,7 +61,8 @@ describe('H5BP Server Configs generator', function () {
 	it('`nginx` option generates expected files', function (cb) {
 		var expected = [ 'h5bp', 'mime.types', 'nginx.conf', 'sites-available', 'sites-enabled' ];
 
-		this.generator.run(['nginx'], function () {
+		this.generator.server = 'nginx';
+		this.generator.run({}, function () {
 			helpers.assertFiles(expected);
 			cb();
 		});
@@ -66,7 +71,31 @@ describe('H5BP Server Configs generator', function () {
 	it('`node` option generates expected files', function (cb) {
 		var expected = [ 'lib', 'package.json' ];
 
-		this.generator.run(['node'], function () {
+		this.generator.server = 'node';
+		this.generator.run({}, function () {
+			helpers.assertFiles(expected);
+			cb();
+		});
+	});
+
+	it('`destination` flag generates files in default location', function (cb) {
+		var expected = [ 'node/lib', 'node/package.json' ];
+
+		this.generator.server = 'node';
+		this.generator.options.destination = true;
+		this.generator.run({}, function () {
+			helpers.assertFiles(expected);
+			cb();
+		});
+
+	});
+
+	it('`destination` flag generates files in custom location', function (cb) {
+		var expected = [ 'custom/lib', 'custom/package.json' ];
+
+		this.generator.server = 'node';
+		this.generator.options.destination = 'custom';
+		this.generator.run({}, function () {
 			helpers.assertFiles(expected);
 			cb();
 		});
